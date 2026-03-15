0846f6e91c540c635547aaf359e24d27039524b8
ShinLucho <shinlucho@gmail.com>
2026-03-15 11:17:12 -0500

    Cambio de color a layout

--- matrix.js
+++ matrix.js
@@ -20,7 +20,7 @@ function draw() {
     ctx.fillStyle = "rgba(0,0,0,0.05)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = "#0ff";
+    ctx.fillStyle = "#71c9ff";
     ctx.font = fontSize + "px monospace";
 
     for (let i = 0; i < drops.length; i++) {
