cbbab7b90bff658892adbd3b738fb77f5f06370c
ShinLucho <shinlucho@gmail.com>
2026-03-15 12:25:14 -0500

    Fix sonarqube issues

--- matrix.js
+++ matrix.js
@@ -4,7 +4,8 @@ const ctx = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-const letters = "アァカサタナハマヤャラワン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const letters =
+  "アァカサタナハマヤャラワン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const matrix = letters.split("");
 
 const fontSize = 14;
@@ -12,30 +13,30 @@ const columns = canvas.width / fontSize;
 
 const drops = [];
 
-for (let x = 0; x < columns; x++)
-    drops[x] = 1;
+for (let x = 0; x < columns; x++) drops[x] = 1;
 
 function draw() {
+  ctx.fillStyle = "rgba(0,0,0,0.05)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = "rgba(0,0,0,0.05)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#71c9ff";
+  ctx.font = fontSize + "px monospace";
 
-    ctx.fillStyle = "#71c9ff";
-    ctx.font = fontSize + "px monospace";
+  for (let i = 0; i < drops.length; i++) {
+    const crypto = globalThis.crypto || globalThis.msCrypto;
+    const randomArray = new Uint32Array(2);
+    crypto.getRandomValues(randomArray);
 
-    for (let i = 0; i < drops.length; i++) {
+    const text = matrix[Math.floor(randomArray[0] % matrix.length)];
 
-        const text = matrix[Math.floor(Math.random() * matrix.length)];
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
 
-        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-
-        if (drops[i] * fontSize > canvas.height && Math.random() > 0.975)
-            drops[i] = 0;
-
-        drops[i]++;
-
+    if (drops[i] * fontSize > canvas.height && randomArray[1] % 100 < 3) {
+      drops[i] = 0;
     }
 
+    drops[i]++;
+  }
 }
 
 setInterval(draw, 35);
